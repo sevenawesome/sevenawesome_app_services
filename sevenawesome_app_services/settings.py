64311c79daef9d32c8e07879aa93449a00c71e22
Salim Bencosme <sevenawesome.srl@gmail.com>
2025-11-02 13:47:04 -0400

    restframework added
    restframework added

--- sevenawesome_app_services/settings.py
+++ sevenawesome_app_services/settings.py
@@ -46,6 +46,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'albums_of_musics',
     'the_churches',
     'people'
 ]
@@ -142,3 +145,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
